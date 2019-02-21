784f91a3c8d04095210d16607daa1f06962b3458
dn <di.an@hotmail.com>
2019-02-21 17:10:11 +0800

    Add  2 outlet offday model

---
+++ PsReservationPortal/Models/OutletOffDayModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PsReservationPortal.Models
+{
+    [Table("OutletOffDay")]
+    public class OutletOffDayModel
+    { 
+        [Key]
+        public int Id { get; set; }
+
+        public int OutletId { get; set; }
+
+        [Display(Name = "Monday")]
+        public bool MonOff { get; set; }
+
+        [Display(Name = "Tuesday")]
+        public bool TueOff { get; set; }
+
+        [Display(Name = "Wednesday")]
+        public bool WedOff { get; set; }
+
+        [Display(Name = "Thursday")]
+        public bool ThuOff { get; set; }
+
+        [Display(Name = "Friday")]
+        public bool FriOff { get; set; }
+
+        [Display(Name = "Saturday")]
+        public bool SatOff { get; set; }
+
+        [Display(Name = "Sunday")]
+        public bool SunOff { get; set; }
+
+        public bool Active { get; set; }
+
+        public CompanyModel Company { get; set; }
+
+        public DateTime ModifiedDateTime { get; set; }
+
+        public int ModifiedUserId { get; set; }
+    }
+}
---
+++ PsReservationPortal/Models/OutletWorkingDateModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PsReservationPortal.Models
+{
+    [Table("OutletWorkingDate")]
+    public class OutletWorkingDateModel
+    { 
+        [Key]
+        public int Id { get; set; }
+
+        public int OutletId { get; set; }
+
+        [Display(Name = "Date From")]
+        public DateTime BusinessDateFrom { get; set; }
+
+        [Display(Name = "Date To")]
+        public DateTime BusinessDateTo { get; set; }
+
+        public bool WorkDateStatus { get; set; }
+
+        public CompanyModel Company { get; set; }
+
+        public DateTime ModifiedDateTime { get; set; }
+
+        public int ModifiedUserId { get; set; }
+    }
+}

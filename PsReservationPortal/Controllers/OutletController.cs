d4d1d718b6f11f692a0f7ab0be932c9d5bbbc8f8
Yusre Tariq Aziz <yusretariqaziz@pointsoft.com.my>
2019-02-25 09:33:32 +0800

    Outlet Controller & View Update

---
+++ PsReservationPortal/Controllers/OutletController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.UI;
+using Microsoft.AspNet.Identity.Owin;
+using PsReservationPortal.Models;
+using PsReservationPortal.ViewModels;
+using Kendo.Mvc.Extensions;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PsReservationPortal.Controllers
+{
+    [Authorize]
+    public class OutletController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            OutletModel outlet = _context.Outlet.FirstOrDefault(a => a.OutletId == id);
+            return View(outlet);
+        }
+
+        public ActionResult Details(int id)
+        {
+            OutletModel outlet = _context.Outlet.FirstOrDefault(a => a.OutletId == id);
+            return View(outlet);
+        }
+    }
+}
